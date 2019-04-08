c474375eb4463fa7f8cd5def082c08642dba07e5
xiaohui <1002714042@qq.com>
2019-04-08 13:59:34 +0800

    Add files via upload

---
+++ move_zero.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sring.h>
+
+void move_zero(int *array, int length)
+{
+	int i=0,j;
+	int count=0;
+
+	while(array[i++]==0){
+		count++;
+		for(j=i; i<length; i++){
+			array[i-1] = array[i];
+		}
+	}
+	
+	memset(array[length-count], 0, count);
+	
+	return ;
+}
+
+void main()
+{
+	int i;
+	
+	for(i=0; i<10; i++){
+		scanf("%d", &array[i]);
+	}
+	move_zero();
+	
+	return ;
+}
